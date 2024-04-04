0452aa67ec615a1a38aa32920ade41d2a942a2be
dev.sosedskiy <dev.sosedskiy@gmail.com>
2024-04-04 12:25:37 +0300

    Задал базовую структуру приложения, написал компонент навбара, задал линки на страницы

--- diplom-frontend/src/app/presentation/presentation-routing.module.ts
+++ diplom-frontend/src/app/presentation/presentation-routing.module.ts
@@ -2,6 +2,9 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {LayoutComponent} from "./layout/layout.component";
 import {MainComponent} from "./pages/main/main.component";
+import {AiScanComponent} from "./pages/ai-scan/ai-scan.component";
+import {SpecialComponent} from "./pages/special/special.component";
+import {ProfileComponent} from "./pages/profile/profile.component";
 
 const routes: Routes = [
   {
@@ -13,6 +16,24 @@ const routes: Routes = [
             pathMatch: 'full',
             component: MainComponent,
             title: 'Главная'
+          },
+          {
+            path: 'ai-scan',
+            pathMatch: 'full',
+            component: AiScanComponent,
+            title: 'AiScan'
+          },
+          {
+            path: 'special',
+            pathMatch: 'full',
+            component: SpecialComponent,
+            title: 'Польза дня'
+          },
+          {
+            path: 'profile',
+            pathMatch: 'full',
+            component: ProfileComponent,
+            title: 'Профиль'
           }
         ]
   }
--- diplom-frontend/src/app/presentation/presentation.module.ts
+++ diplom-frontend/src/app/presentation/presentation.module.ts
@@ -1,19 +1,34 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {LayoutComponent} from "./layout/layout.component";
 import {RouterOutlet} from "@angular/router";
-import {MainComponent} from "./pages/main/main.component";
+
 import {PresentationRoutingModule} from "./presentation-routing.module";
+
+import {AvatarModule} from "primeng/avatar";
+
+import {LayoutComponent} from "./layout/layout.component";
+import {MainComponent} from "./pages/main/main.component";
+import {NavMenuBarComponent} from "./components/nav-menu-bar/nav-menu-bar.component";
+import {RandomColorPipe} from "./pipes/random-color/random-color.pipe";
+import { AiScanComponent } from './pages/ai-scan/ai-scan.component';
+import { SpecialComponent } from './pages/special/special.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
     LayoutComponent,
     MainComponent,
+    NavMenuBarComponent,
+    AiScanComponent,
+    SpecialComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
     RouterOutlet,
-    PresentationRoutingModule
+    PresentationRoutingModule,
+    RandomColorPipe,
+    AvatarModule,
   ],
   bootstrap: []
 })
